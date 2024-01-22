caee078fa8731d08f35202a674de474505edf0d2
nikita829 <nikita96587@gmail.com>
2024-01-22 20:09:16 +0500

    2

---
+++ Diapason/Diapason.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <windows.h>
+
+class Diapason {
+	static int counter;
+public:
+    // 1 задание
+    Diapason() {
+        counter++; // увеличиваем счетчик при каждом создании объекта
+    }
+    ~Diapason() {
+        counter--; // уменьшаем счетчик при каждом удалении объекта
+    }
+    static int getCounter() {
+        return counter; // метод для получения текущего значения счетчика
+    }
+	double x;
+	double y;
+	bool intersection(Diapason d1, Diapason d2) {
+        bool b = true;
+        double x1, y1, x2, y2;
+        x1 = min(d1.x, d1.y);
+        y1 = max(d1.x, d1.y);
+        x2 = min(d2.x, d2.y);
+        y2 = max(d2.x, d2.y);
+        if (x1 < x2 && y1 < x2)
+        {
+            b = false;
+        }
+        else
+        {
+            if (x2 < x1 && y2 < x1)
+            {
+                b = false;
+            }
+        }
+        return b;
+	}
+    // 2 задание
+    double size() {
+        double Max = max(x, y);
+        double Min = min(x, y);
+        if (Max<0){
+            Max = abs(Max);
+            Min = abs(Min);
+            std::swap(Max, Min);
+        }
+        return Max - Min;
+    }
+    int Convert(double x) {
+        return (int)x;  // неявное приведение типа double к int (целая часть)
+    }
+    double Convert(int y) {
+        return (double)y;  // явное приведение типа int к double
+    }
+    double binar_operation(Diapason D, int num) {
+        D.x -= num;
+        return D.x;
+    }
+    double binar_operation(int num, Diapason D) {
+        for (int i = 0; i < num; i++)
+        {
+            D.y--;
+        }
+        return D.y;
+    }
+    bool binar_operation(Diapason D, Diapason d) {
+        return (intersection(D, d));
+    }
+};
+
+int Diapason::counter = 0;
---
+++ Diapason/DiapasonArray.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include "F:\Учеба\2 курс\ТАиСД\Новая папка\Diapason\Diapason.cpp"
+using namespace std;
+
+class DiapasonArray {
+	static int count;
+	double Max = DBL_MIN;
+	int MaxEl;
+public:
+	int SizeArr;
+	Diapason* arr;
+
+	DiapasonArray() {
+		count++;
+	}
+
+	DiapasonArray(int size) {
+		count++;
+		SizeArr = size;
+		arr = new Diapason[SizeArr];
+		for (int i = 0; i < size; i++)
+		{
+			cout << "Введите начало диапазона " << i + 1 << ": ";
+			cin >> arr[i].x;
+			cout << "Введите конец  диапазона " << i + 1 << ": ";
+			cin >> arr[i].y;
+		}
+	}
+
+	DiapasonArray(int size, int min, int max) {
+		count++;
+		SizeArr = size;
+		arr = new Diapason[SizeArr];
+		for (int i = 0; i < size; i++)
+		{
+			arr[i].x = rand() % (max - min) + min;
+			arr[i].y = rand() % (max - min) + min;
+		}
+	}
+
+	~DiapasonArray() {
+		count--; 
+	}
+
+	Diapason Max_Element() {
+		for (int i = 0; i < SizeArr; i++)
+		{
+			if (Max < arr[i].x) {
+				Max = arr[i].x;
+				MaxEl = i;
+			}
+			else
+			{
+				if (Max < arr[i].y) {
+					Max = arr[i].y;
+					MaxEl = i;
+				}
+			}
+		}
+		return arr[MaxEl];
+	}
+
+	double MaxNum() {
+		return max(Max_Element().x, Max_Element().y);
+	}
+
+	Diapason NumOut(int i) {
+		return arr[i-1];
+	}
+
+	void ArrayOut() {
+		for (int i = 0; i < SizeArr; i++)
+		{
+			cout << arr[i].x << "\t...\t" << arr[i].y << "\n";
+		}
+	}
+
+	static int GetCount() {
+		return count; 
+	}
+};
+
+int DiapasonArray::count = 0;
